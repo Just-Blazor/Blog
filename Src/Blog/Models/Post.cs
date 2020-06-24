f1483fb0b0632f3012912bb4a5c4513ed16789a8
Faber Leonardo Sanchez Hidalgo <FaberSanZ@users.noreply.github.com>
2020-06-24 18:40:56 -0500

    Update [Blog]

---
+++ Src/Blog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; } // 
+        public DateTime Day { get; set; } = DateTime.Now;
+    }
+}
